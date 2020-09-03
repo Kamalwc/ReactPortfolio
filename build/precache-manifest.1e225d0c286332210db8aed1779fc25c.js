fe5b84cfe815694bb4303266de0fe450b5db1fef
kamalwc <carmichaelkw@gmail.com>
2020-09-03 09:02:07 -0400

    changed landing page button link to projects

---
+++ build/precache-manifest.1e225d0c286332210db8aed1779fc25c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc712dbdc3813748cab5953b4527af05",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "b51ece0be89ffc97085d",
+    "url": "/ReactPortfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d0dcddc56d83ade1a770",
+    "url": "/ReactPortfolio/static/js/2.8bea8942.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ReactPortfolio/static/js/2.8bea8942.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b51ece0be89ffc97085d",
+    "url": "/ReactPortfolio/static/js/main.c6450a7a.chunk.js"
+  },
+  {
+    "revision": "97a1c7d0b592075495b7",
+    "url": "/ReactPortfolio/static/js/runtime-main.ad87311a.js"
+  }
+]);
